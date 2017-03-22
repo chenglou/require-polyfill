f4907923757459821f7175abcbc77f6a504eceb9
Cheng Lou <chenglou92@gmail.com>
2017-03-22 03:08:54 -0700

    Fix leftpad example
    Yeah I know, I screwed up trying to use leftpad.

--- example/index.js
+++ example/index.js
@@ -1,5 +1,3 @@
 var leftPad = require('left-pad');
 
-document.getElementById('myDiv').innerText = '|' + leftPad('hello world', 10);
-console.log('ab' + leftPad('hello', 5, 'h'))
-console.log(leftPad)
+document.getElementById('myDiv').innerText = leftPad('hello world', 15, 'h');
